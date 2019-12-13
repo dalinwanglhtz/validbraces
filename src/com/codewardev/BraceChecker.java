cb57444aa7ef3004fd45a1defc9bd4f313dd9212
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-13 22:44:04 +1100

    added while loop to iterate over the processing of string remains

--- src/com/codewardev/BraceChecker.java
+++ src/com/codewardev/BraceChecker.java
@@ -11,19 +11,24 @@ public class BraceChecker {
 		String braces="()";
 		
 
+		while(true) {
 			if(string.contains(braces)) {
 				string = string.replace(braces, "");
+				continue;
 			}
 			System.out.println("String: "+string);
 			if(string.contains(cBraces)) {
 				string = string.replace(cBraces, "");
+				continue;
 			}
 			System.out.println("String: "+string);
 			if(string.contains(brackets)) {
 				string = string.replace(brackets, "");
+				continue;
 			}
 			System.out.println("String: "+string);
-
+			break;
+		}
 		
 		if(string.isEmpty()) {
 			return true;
